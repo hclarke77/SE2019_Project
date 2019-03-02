f95b93032f30d9d1500ae769d6ad7807448c7c3d
hclarke77 <clarkehayden0@gmail.com>
2019-03-02 14:49:44 -0600

    added buffer

--- Buffer.java
+++ Buffer.java
@@ -5,9 +5,37 @@ import java.util.*;
 public class Buffer {
 
     int size;
-    csvReader reader;
+    List<String> rates,times;
     
-    public Buffer(int size, csvReader reader) {
     
+    public Buffer(int s, List<String> r) {
+        
+        size = s;
+        rates = r;     
     }
+         
+    public List<String> loadTimesBuffer(csvReader reader) {
+    
+        List<List<String>> times;
+        
+        times = reader.exportVariables();
+        
+        List<String> newBuffer = times.get(0);
+        
+        return newBuffer;
+        
+        }
+    
+         
+    public List<String> loadRatesBuffer(csvReader reader) {
+    
+        List<List<String>> rates;
+        
+        rates = reader.exportVariables();
+        
+        List<String> newBuffer = rates.get(1);
+        
+        return newBuffer;
+        
+        }
 }
