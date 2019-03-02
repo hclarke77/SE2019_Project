64b1a50f7e199f9b63e9fe598ae1f1a878769b9a
reillynl <noah.reilly@slu.edu>
2019-03-02 15:32:44 -0600

    Add files via upload

--- Simulation.java
+++ Simulation.java
@@ -8,16 +8,23 @@ public class Simulation {
   public static void main(String[] args) {
 	  //Asks user for file name they wish to simulate
 	  Scanner scanner = new Scanner (System.in);
-	  System.out.print("Enter your File name: ");  
+	  System.out.println("Enter your File name: ");  
 	  String Filename = scanner.next();
 	  
 	  //Calls csv Reader on user's File
-	  csvReader(Filename) = reader;
-	  reader.exportVariables();
+	  csvReader reader = new csvReader(Filename);
+	  
+	  //Gets the date and time values from the csv File
+	  Buffer ListTimes = new Buffer(reader);
+	  ListTimes.loadTimesBuffer(reader);
+	  
+	  //Gets the rate values from the csv File
+	  Buffer ListRates = new Buffer(reader);
+	  ListRates.loadRatesBuffer(reader);
+	  
+	  //ProccesingUnit(double speed, Buffer B)
+	  
 		
   }
-
-
-
 
 }
