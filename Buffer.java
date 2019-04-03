3f9c21db1b1736455e72b737c627138122284eec
Romeo Scagliarini <romeoscagliarini@yahoo.com>
2019-04-03 15:52:34 -0500

    Buffer add/process changes

--- Buffer.java
+++ Buffer.java
@@ -8,44 +8,59 @@ long time;
 long messagesProcessed;
 long numMessages;
 long bufferSize;
+long numberLost;
+long maxNumberLost;
+long totalNumberLost;
 Queue<Message> qMess;
 
 public Buffer(long size) {
 
+  bufferSize = size;
   Queue qMess = Queue<Message>(bufferSize);
+  maxNumberLost = 0;
+  totalNumberLost = 0;
 
 }
 
 public void addMessages(long rate, long currTime) {
   time = currTime;
   numMessages = rate;
+  numberLost = 0;
   for(int i=0;i<numMessages;i++) {
-    qMess.add(new Message(time));
-
+    if (qMess.size() < bufferSize) {
+      qMess.add(new Message(time));
+    }
+    else {
+      numberLost += 1;
+      totalNumberLost += 1;
+      if (numberLost >= maxNumberLost) {
+        maxNumberLost = numberLost;
+      }
+    }
   }
 }
 
 public void processMessages(long rate, long currTime) {
   time = currTime;
-  numMessages = rate;
-
-
-
-
-
-
-
-
-
-
+  messagesProcessed = rate;
+  for(int i=0;i<numMessages;i++) {
+      Message x = qMess.remove();
+      x.setOut(time);
+  }
 }
 
 
 
-public int numberDropped() {
 
-  int dropped;
-  dropped =
+public long maxNumberDropped(){
+
+  return maxNumberLost;
+
+}
+
+public long totalNumberDropped() {
+
+  return totalNumberLost;
 
 }
 
